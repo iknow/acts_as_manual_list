7be0e685f9a19f892bdb5a35faaaf9ce09d933d2
Chris Andreae <chris@bibo.com.ph>
2018-10-29 13:50:07 +0900

    update_positions: return number of positions changed

--- lib/acts_as_manual_list.rb
+++ lib/acts_as_manual_list.rb
@@ -1,17 +1,20 @@
+# frozen_string_literal: true
+
 require 'lazily'
 require 'iknow_list_utils'
 
 module ActsAsManualList
   using IknowListUtils
 
-  POSITION_GETTER = ->(el   ){ el.send(:position) }
-  POSITION_SETTER = ->(el, v){ el.send(:position=, v) }
+  POSITION_GETTER = ->(el)    { el.send(:position) }
+  POSITION_SETTER = ->(el, v) { el.send(:position=, v) }
 
   # Takes an list of elements in a desired order. Elements have an inherent
   # float position, obtainable via `position_getter`, which may be nil if the
   # element previously was not a list member. For each element whose position
   # is not in order (i.e. between the positions of its neighbours), calls
   # `position_setter` to update the position to a new float value.
+  # Returns the count of elements whose positions were changed.
   def self.update_positions(elements,
                             position_getter: POSITION_GETTER,
                             position_setter: POSITION_SETTER)
@@ -38,16 +41,20 @@ module ActsAsManualList
     # For each possible range of indices that are not stable (i.e. for every
     # adjacent pair of stable indices), assign new positions distributed
     # between the stable positions.
+    changed_positions = 0
     stable_position_indices.each_cons(2) do |(start_pos, start_index), (end_pos, end_index)|
       range = (start_index + 1)..(end_index - 1)
-      next unless range.size > 0
+      next if range.size.zero?
 
+      changed_positions += range.size
       new_positions = select_positions(start_pos, end_pos, range.size)
 
       new_positions.each.with_index(1) do |new_pos, offset|
         position_setter.(elements[start_index + offset], new_pos)
       end
     end
+
+    changed_positions
   end
 
   # Select `count` positions in order between the provided `start_pos` and `end_pos`
@@ -66,7 +73,6 @@ module ActsAsManualList
       delta = (end_pos - start_pos) / (count + 1)
       1.upto(count).map { |i| start_pos + (delta * i) }
     end
-
   end
 end
 
@@ -82,6 +88,6 @@ if defined?(Rails::Railtie)
       end
     end
   end
-else
-  ActiveRecord::Base.send(:include, ActsAsManualList::Base) if defined?(ActiveRecord)
+elsif defined?(ActiveRecord)
+  ActiveRecord::Base.send(:include, ActsAsManualList::Base)
 end
